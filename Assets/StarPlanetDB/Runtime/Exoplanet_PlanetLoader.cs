a62dbef389cb4b2e11e830e936c3396f35f6c231
Jerry Isdale <isdale@gmail.com>
2018-10-21 00:45:41 -1000

    First Commit
    Added Project files into repo

---
+++ Assets/StarPlanetDB/Runtime/Exoplanet_PlanetLoader.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exoplanet_PlanetLoader : MonoBehaviour {
+    public Exoplanet_Data_Planets planets;
+
+	// Use this for initialization
+	void Start () {
+        Debug.LogFormat("Planet {0}", planets.dataArray[0].Pl_name);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/StarPlanetDB/Runtime/Exoplanet_StarsLoader.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exoplanet_StarsLoader : MonoBehaviour {
+    public Exoplanet_StarsUnique2 stars;
+
+	// Use this for initialization
+	void Start () {
+        Debug.LogFormat("Star {0}", stars.dataArray[0].Pl_hostname);
+	}
+	
+}
